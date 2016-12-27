b7b93789e87c5eacbdfd16250d0a749514b83def
yintothayang <yintothayang@gmail.com>
2016-12-26 23:42:04 -0500

    remove testing code from main

--- src/main.js
+++ src/main.js
@@ -2,18 +2,3 @@ import LudicConnect from './LudicConnect'
 window.ludicConnnect = LudicConnect;
 
 
-setTimeout(() => {
-  let createLobbyButton = document.getElementById('createLobby');
-  let findLobbiesButton = document.getElementById('findLobbies');
-
-  createLobbyButton.addEventListener('click', function(){LudicConnect.createLobby("testLobby")}, false);
-
-
-  findLobbiesButton.addEventListener('click', function(){LudicConnect.findLobbies("testLobby").then(lobbies => {
-    console.log("all lobbies: ", lobbies);
-    let currentLobby = lobbies[lobbies.length - 1];
-    console.log("joining: ", currentLobby);
-    LudicConnect.joinLobby(currentLobby.id);
-  })}, false);
-}, 55);
-
